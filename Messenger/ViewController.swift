574a1c036a0be1f25aac7224e75c53716f49a229
Bogdan Belogurov <belbog@me.com>
2018-09-22 22:01:38 +0300

    Initial Commit

---
+++ Messenger/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  Messenger
+//
+//  Created by Bogdan Belogurov on 20/09/2018.
+//  Copyright © 2018 Bogdan Belogurov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
