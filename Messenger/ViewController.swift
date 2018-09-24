40fac651d440ceb9474e02eafaa16fe3ae68fa62
Bogdan Belogurov <belbog@me.com>
2018-09-24 22:20:57 +0300

    Add icons and logs

--- Messenger/ViewController.swift
+++ Messenger/ViewController.swift
@@ -9,9 +9,46 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    private func logViewController (_ superFunc: String) {
+        guard Log().logEnabled else {
+            return
+        }
+        print(superFunc)
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        logViewController(#function)
     }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        logViewController(#function)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        logViewController(#function)
+    }
+    
+    override func viewWillLayoutSubviews() {
+        logViewController(#function)
+    }
+    
+    override func viewDidLayoutSubviews() {
+        logViewController(#function)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        logViewController(#function)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        logViewController(#function)
+    }
+
 }
 
