0649e495f1875f4912b1464f5c8a263827c20263
Bogdan Belogurov <belbog@me.com>
2018-09-30 21:20:30 +0300

    Add ProfileViewController and imagePicker

--- Messenger/AppDelegate.swift
+++ Messenger/AppDelegate.swift
@@ -13,59 +13,32 @@ import CoreData
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    
-    private var pastState: String = "App is inactive"
-
-    private func logging(_ superFunc: String) {
-        guard Log().logEnabled else {
-            return
-        }
-        switch UIApplication.shared.applicationState {
-        case .active:
-            print("App moved from \(pastState) to active: \(superFunc)")
-            pastState = "App is active"
-        case .background:
-            print("App moved from \(pastState) to background: \(superFunc)")
-            pastState = "App is background"
-        case .inactive:
-            print("App moved from \(pastState) to inactive: \(superFunc)")
-            pastState = "App is inactive"
-            break
-        }
-        
-    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        logging(#function)
         return true
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
-        logging(#function)
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-        logging(#function)
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
-        logging(#function)
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-        logging(#function)
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
         // Saves changes in the application's managed object context before the application terminates.
-        logging(#function)
         self.saveContext()
     }
 
---
+++ Messenger/ProfileViewController.swift
@@ -0,0 +1,113 @@
+//
+//  ProfileViewController.swift
+//  Messenger
+//
+//  Created by Bogdan Belogurov on 28/09/2018.
+//  Copyright © 2018 Bogdan Belogurov. All rights reserved.
+//
+
+import UIKit
+
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBOutlet weak var userImage: UIImageView!
+    
+    @IBOutlet weak var editButton: UIButton!
+    
+    @IBOutlet weak var newImageButton: UIButton!
+    
+    let imagePicker = UIImagePickerController()
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+       // print(editButton.frame) //выдает ошибку так как обект класса еще не создался и метод sizeAndLayoutSettings еще не вызван.
+    }
+    
+    @IBAction func selectNewImage(_ sender: Any) {
+        print("Выбери изображение профиля")
+        setAlertControllerForSelectNewImage()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        testFrameEditButton()
+        sizeAndLayoutSettings()
+        imagePicker.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        testFrameEditButton()
+    }
+    
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+    }
+    
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+    }
+    
+    func sizeAndLayoutSettings() {
+        
+        userImage.layer.cornerRadius = 30
+        userImage.clipsToBounds = true
+        
+        newImageButton.layer.cornerRadius = 30
+        newImageButton.clipsToBounds = true
+        
+        editButton.layer.cornerRadius = 20
+        editButton.layer.borderWidth = 1.0
+        editButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
+        editButton.clipsToBounds = true
+    }
+
+    private func testFrameEditButton(caller: String = #function) {
+        print("Caller func \(caller), frame edit button = \(editButton.frame)")
+    }
+    
+    private func  setAlertControllerForSelectNewImage() {
+        let alertController = UIAlertController(title: NSLocalizedString("Истоник фотографий", comment: "Истоник фотографий"), message: nil, preferredStyle: .actionSheet)
+        let cameraAction = UIAlertAction(title: NSLocalizedString("Камера", comment: "Камера") , style: .default, handler: { (action) in
+            self.imagePicker.allowsEditing = true
+            self.imagePicker.sourceType = .camera
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        })
+        let photoLibAction = UIAlertAction(title: NSLocalizedString("Фото", comment: "Фото"), style: .default, handler: { (action) in
+            self.imagePicker.allowsEditing = true
+            self.imagePicker.sourceType = .photoLibrary
+            
+            self.present(self.imagePicker, animated: true, completion: nil)
+        })
+        
+        let cancelAction = UIAlertAction(title: NSLocalizedString("Отмена", comment: "Отмена"), style: .cancel, handler: nil)
+        
+        alertController.addAction(cameraAction)
+        alertController.addAction(photoLibAction)
+        alertController.addAction(cancelAction)
+        self.present(alertController, animated: true, completion: nil)
+    }
+    
+    //MARK: - Delegates For Image Picker
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let chosenImage = info[.originalImage] as? UIImage {
+        userImage.contentMode = .scaleToFill
+        userImage.image = chosenImage
+        }
+        dismiss(animated:true, completion: nil)
+    }
+    
+}
