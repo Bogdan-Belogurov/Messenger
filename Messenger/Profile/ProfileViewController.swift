9e1469ced1d4e4af7b9c430a0f60089e39150372
Bogdan Belogurov <belbog@me.com>
2018-11-01 21:18:29 +0300

    Change keyboard profileInfo, add CoreDataStack and entities

--- Messenger/Profile/ProfileViewController.swift
+++ Messenger/Profile/ProfileViewController.swift
@@ -6,25 +6,24 @@
 //  Copyright © 2018 Bogdan Belogurov. All rights reserved.
 //
 
-import UIKit
 import CoreData
+import UIKit
 
-class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
-    
-    @IBOutlet weak var userImage: UIImageView!
-    
+class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    @IBOutlet var userImage: UIImageView!
+
     @IBOutlet var editButton: UIButton!
     @IBOutlet var saveStackView: UIStackView!
-    @IBOutlet weak var newImageButton: UIButton!
+    @IBOutlet var newImageButton: UIButton!
     @IBOutlet var nameTextField: UITextField!
     @IBOutlet var descriptionTextField: UITextField!
     @IBOutlet var nameProfileLabel: UILabel!
     @IBOutlet var descriptonProfileLabel: UILabel!
     @IBOutlet var actitvityIndicator: UIActivityIndicatorView!
-    
+
     private var isEditMode: Bool = false
     var dataSavePicker: SaveProfileProtocol = GCDDataManager()
-    var profile: UserProfile = UserProfile(name: "Name", description: "Description", image: UIImage(named: "placeholder-user")) 
+    var profile: UserProfile = UserProfile(name: "Name", description: "Description", image: UIImage(named: "placeholder-user"))
 
     @IBOutlet var gcdButton: UIButton!
     @IBOutlet var operationButton: UIButton!
@@ -32,133 +31,87 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         gcdButton.isEnabled = true
         operationButton.isEnabled = true
     }
-    
-    func printDataFromCore() {
-        
-        let request = NSFetchRequest<NSFetchRequestResult>(entityName: "UserProfileCore")
-        if let context = (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.persistentContainer.viewContext {
-        request.returnsObjectsAsFaults = false
-        do {
-            let result = try context.fetch(request)
-            for data in result as! [NSManagedObject] {
-                print(data.value(forKey: "userName") as! String)
-                print(data)
-            }
-            
-        } catch {
-            
-            print("Failed")
-        }
-        }
-    }
-    
+
     @IBAction func saveButtonPressed(_ sender: UIButton) {
-        
-        if let context = (UIApplication.shared.delegate as? AppDelegate)?.coreDataStack.persistentContainer.viewContext {
-            let dataFromCore = UserProfileCore(context: context)
-            dataFromCore.userName = nameTextField.text
-            dataFromCore.userDescription = descriptionTextField.text
-            do {
-                
-                try context.save()
-                print("Сохранение  удалось!")
-            } catch let error as NSError {
-                print("Не удалось сохранить данные \(error), \(error.userInfo)")
-            }
-            
-            printDataFromCore()
-        }
-        
-        
-        
-        
         actitvityIndicator.isHidden = false
         actitvityIndicator.startAnimating()
         profile.name = nameTextField.text
         profile.description = descriptionTextField.text
         profile.image = userImage.image
-        
+
         let titleOfButton = sender.titleLabel?.text
-        
+
         if titleOfButton == "GCD" {
             dataSavePicker = GCDDataManager()
         } else {
             dataSavePicker = OperationDataManager()
         }
-        
-        self.dataSavePicker.saveUserProfile(userProfile: profile) { (success) in
+
+        dataSavePicker.saveUserProfile(userProfile: profile) { success in
             self.actitvityIndicator.stopAnimating()
             self.actitvityIndicator.isHidden = true
-            
+
             if success {
                 self.successAlert()
                 self.loadDataFromUserDefaults()
             } else {
-                self.errorAlert(function: {self.saveButtonPressed(sender)})
+                self.errorAlert(function: { self.saveButtonPressed(sender) })
             }
         }
-        
+
         viewEditor(isEditMode)
     }
-    
-    
+
     @IBAction func editPressed(_ sender: Any) {
         viewEditor(isEditMode)
         gcdButton.isEnabled = false
         operationButton.isEnabled = false
     }
-    
+
     @IBAction func dismissVC(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
-    
+
     let imagePicker = UIImagePickerController()
-    
+
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
-    
+
     @IBAction func selectNewImage(_ sender: Any) {
         print("Выбери изображение профиля")
         setAlertControllerForSelectNewImage()
     }
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-       // testFrameEditButton()
+        // testFrameEditButton()
         imagePicker.delegate = self
         loadDataFromUserDefaults()
         viewEditor(isEditMode)
-        observeKeyboardNotifications()
+        addKeyboardObserver()
     }
-    
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
-    }
-    
-    
+
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
-        self.sizeAndLayoutSettings()
+        sizeAndLayoutSettings()
     }
-    
+
     func sizeAndLayoutSettings() {
         userImage.layer.cornerRadius = 30
         userImage.clipsToBounds = true
-        
+
         newImageButton.layer.cornerRadius = 30
         newImageButton.clipsToBounds = true
-        
+
         editButton.layer.cornerRadius = 20
         editButton.layer.borderWidth = 1.0
         editButton.layer.borderColor = #colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)
         editButton.clipsToBounds = true
-        
+
         actitvityIndicator.isHidden = true
-        
     }
-    
+
     private func viewEditor(_ state: Bool) {
         nameProfileLabel.isHidden = state
         descriptonProfileLabel.isHidden = state
@@ -169,15 +122,15 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
         editButton.isHidden = state
         isEditMode = !isEditMode
     }
-    
-    private func  setAlertControllerForSelectNewImage() {
+
+    private func setAlertControllerForSelectNewImage() {
         let alertController = UIAlertController(title: NSLocalizedString("Истоник фотографий", comment: "Истоник фотографий"), message: nil, preferredStyle: .actionSheet)
-        let cameraAction = UIAlertAction(title: NSLocalizedString("Камера", comment: "Камера") , style: .default, handler: { (action) in
-            
+        let cameraAction = UIAlertAction(title: NSLocalizedString("Камера", comment: "Камера"), style: .default, handler: { _ in
+
             if !(UIImagePickerController.isSourceTypeAvailable(.camera)) {
-                let warningAlert = UIAlertController(title: "Ошибка!", message: "Невозможно использовать камеру", preferredStyle: .alert);
+                let warningAlert = UIAlertController(title: "Ошибка!", message: "Невозможно использовать камеру", preferredStyle: .alert)
                 let closeAction = UIAlertAction(title: "Ок", style: .cancel, handler: nil)
-                
+
                 warningAlert.addAction(closeAction)
                 self.present(warningAlert, animated: true, completion: nil)
             } else {
@@ -186,51 +139,52 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
                 self.present(self.imagePicker, animated: true, completion: nil)
             }
         })
-        let photoLibAction = UIAlertAction(title: NSLocalizedString("Фото", comment: "Фото"), style: .default, handler: { (action) in
+        let photoLibAction = UIAlertAction(title: NSLocalizedString("Фото", comment: "Фото"), style: .default, handler: { _ in
             self.imagePicker.allowsEditing = true
             self.imagePicker.sourceType = .photoLibrary
-            
+
             self.present(self.imagePicker, animated: true, completion: nil)
         })
-        
+
         let cancelAction = UIAlertAction(title: NSLocalizedString("Отмена", comment: "Отмена"), style: .cancel, handler: nil)
-        
+
         alertController.addAction(cameraAction)
         alertController.addAction(photoLibAction)
         alertController.addAction(cancelAction)
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
-    
+
     private func successAlert() {
         let alertController = UIAlertController(title: "Changes saved!", message: nil, preferredStyle: .alert)
         alertController.addAction(UIAlertAction(title: "Done", style: .default, handler: nil))
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
-    
-    private func errorAlert(function: @escaping () -> ()) {
+
+    private func errorAlert(function: @escaping () -> Void) {
         let alertController = UIAlertController(title: "Error", message: nil, preferredStyle: .alert)
         let repeatAction = UIAlertAction(title: "Save again", style: .default, handler: {
-            (alert: UIAlertAction!) in function()
+            (_: UIAlertAction!) in function()
         })
-        
+
         alertController.addAction(repeatAction)
         alertController.addAction(UIAlertAction(title: "Done", style: .cancel, handler: nil))
-        self.present(alertController, animated: true, completion: nil)
+        present(alertController, animated: true, completion: nil)
     }
-    
-    //MARK: - Delegates For Image Picker
-    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+
+    // MARK: - Delegates For Image Picker
+
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey: Any]) {
         if let chosenImage = info[.originalImage] as? UIImage {
-        userImage.contentMode = .scaleToFill
-        userImage.image = chosenImage
+            userImage.contentMode = .scaleToFill
+            userImage.image = chosenImage
         }
-        dismiss(animated:true, completion: nil)
+        dismiss(animated: true, completion: nil)
     }
-    
-    //MARK: - Save and Load Profile
-    
+
+    // MARK: - Save and Load Profile
+
     func loadDataFromUserDefaults() {
-        self.dataSavePicker.loadUserProfile { (profile) in
+        dataSavePicker.loadUserProfile { profile in
             self.nameProfileLabel.text = profile?.name
             self.descriptonProfileLabel.text = profile?.description
             self.nameTextField.text = profile?.name
@@ -238,28 +192,39 @@ class ProfileViewController: UIViewController, UIImagePickerControllerDelegate, 
             self.userImage.image = profile?.image
         }
     }
-    
-    fileprivate func observeKeyboardNotifications()
-    {
-        let notifier = NotificationCenter.default
-        notifier.addObserver(self, selector: #selector(keyboardShow), name: UIWindow.keyboardWillShowNotification, object: nil)
-        notifier.addObserver(self, selector: #selector(keyboardHide), name: UIWindow.keyboardWillHideNotification, object: nil)
+
+    private func addKeyboardObserver() {
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillShow(notification:)),
+                                               name: UIResponder.keyboardWillShowNotification,
+                                               object: nil)
+        NotificationCenter.default.addObserver(self,
+                                               selector: #selector(keyboardWillHide(notification:)),
+                                               name: UIResponder.keyboardWillHideNotification,
+                                               object: nil)
     }
-    
-    @objc func keyboardShow() {
-        
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-            self.view.frame = CGRect (x: 0, y: -(self.userImage.frame.height-50), width: self.view.frame.width, height: self.view.frame.height)
-        }, completion: nil)
-        
+
+    @objc func keyboardWillShow(notification: NSNotification) {
+        if view.frame.origin.y == 0,
+            let keyboardFrame = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let keyboardHeight = keyboardFrame.size.height
+            view.frame = CGRect(x: view.frame.origin.x,
+                                y: -(keyboardHeight),
+                                width: view.frame.width,
+                                height: view.frame.height)
+            view.layoutIfNeeded()
+        }
     }
-    
-    
-    @objc func keyboardHide() {
-        UIView.animate(withDuration: 0.5, delay: 0, usingSpringWithDamping: 1, initialSpringVelocity: 1, options: .curveEaseInOut, animations: {
-            self.view.frame = CGRect (x: 0, y: 0, width: self.view.frame.width, height: self.view.frame.height)
-        }, completion: nil)
-        
+
+    @objc func keyboardWillHide(notification: NSNotification) {
+        view.frame = CGRect(x: view.frame.origin.x,
+                            y: 0,
+                            width: view.frame.width,
+                            height: view.frame.height)
+        view.layoutIfNeeded()
     }
-    
+
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        view.endEditing(true)
+    }
 }
